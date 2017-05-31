8afe137f1722af642a64e8891536df578b9e6000
Matt Lomet <mattlomet@live.com>
2017-05-31 16:46:32 -0400

    adding some things to template

--- models.rb
+++ models.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
-    has_many :things
-    has_one :job
-    belongs_to :organization
     
     validates_presence_of :username, :password
     validates :username, uniqueness: true
