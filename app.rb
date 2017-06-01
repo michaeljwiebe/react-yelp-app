49b23072b1532810b37b100d86d8ccc2cf9a7945
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-06-01 14:18:13 -0400

    added logout button

--- app.rb
+++ app.rb
@@ -36,13 +36,16 @@ get '/business_finder' do
 end
 
 post '/favorite_businesses' do
-  p params
   favorite_business = Business.create(business_name: params[:business_name],business_location: params[:business_location],business_phone: params[:business_phone],user_id: session[:user_id])
-  p favorite_business
   favorite_business.to_json
 end
 
 get '/favorites' do
   @businesses = Business.where(user_id: session[:user_id]);
   erb :favorites
+end
+
+get '/sign_out' do
+    session[:user_id] = nil
+    redirect '/'
 end
